3bade2a4102221f8752733a6526798a28ef46390
mervezgin <mervezgin@gmail.com>
2024-04-16 12:29:42 +0300

    added speed and rpm info

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody playerRb;
 
-    [SerializeField] float horsePower = 0f;
     [SerializeField] GameObject CenterOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
 
-    //float speed = 15.0f;
+    float horsePower = 25000.0f;
     float turnSpeed = 20.0f;
     float horizontalInput;
     float verticalInput;
+    float speed;
+    float rpm;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +30,12 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
+        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);
+        speedometerText.SetText("Speed: " + speed + " mph");
+
+        rpm = (speed % 30) * 40;
+        rpmText.SetText("RPM: " + rpm);
 
         playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
         //transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
